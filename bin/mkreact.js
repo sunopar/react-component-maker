8f0b75d019112e6272096c039a19dc3748d8118b
sunopar <xygjsj@139.com>
2017-03-17 22:15:17 +0800

    use es6 and optimize

--- bin/mkreact.js
+++ bin/mkreact.js
@@ -1,42 +1,53 @@
 #!/usr/bin/env node
-
-'use strict';
-
-// var createReactComponent = require('react-component-maker')
-var createReactComponent = require('../lib/createReactComponent');
-var program = require('commander');
-
-function parseVal(val) {
-    return val.split(',');
+'use strict'
+const createReactComponent = require('../lib/createReactComponent')
+const program = require('commander')
+function parseVal (val) {
+  return val.split(',')
 }
+function log (name, componentStatus, style) {
+  console.log(name)
+  console.log(`you will create ${componentStatus}component ${name} with ${style}`)
+}
+// if use pure and scss or less both, it can find the right name
+function getName(name,style){
+  const newName = typeof program.pure !== 'boolean' && program.pure? name : program[style]
+  return newName;
+}
+(function () {
+  let usePureComponent = false
+  let componentStatus = ''
+  let style = 'css'
+  let name
 
-program
+  program
     .version('1.2.0')
-    .option('-s --Scss [names]', 'replace css to Scss', parseVal)
-    .option('-l --Less [names]', 'replace css to Less', parseVal)
-    .option('-p --pure [names]', 'use pure component',parseVal)
-    .parse(process.argv);
-var usePureComponent = false
-if(program.pure){
-    usePureComponent = true
-    console.log('you will create pure component for stateless function')
-}
-var name = typeof program.pure !== 'boolean' && program.pure
-if (program.Scss) {
-    name = name?name:program.Scss
-    console.log(name);
-    console.log('you will create components ' + name + ' with Scss');
-} else if (program.Less) {
-    name = name?name:program.Less    
-    console.log(name);
-    console.log('you will create components ' + name + ' with Less');
-} else {
-    name = process
-        .argv[2]
-        .split(',');
-    console.log(name);
-    console.log('you will create components ' + name + ' with css');
-}
-    createReactComponent(name,'css',usePureComponent);
+    .option('-s --scss [names]', 'replace css to Scss', parseVal)
+    .option('-l --less [names]', 'replace css to Less', parseVal)
+    .option('-p --pure [names]', 'use pure component', parseVal)
+    .parse(process.argv)
 
-console.log('done!');
+  if (program.pure) {
+    usePureComponent = true
+    componentStatus = 'pure '
+    name = program.pure
+  }
+  if (program.scss) {
+    style = 'scss'
+    name = getName(name,style)
+    log(name, componentStatus, style)
+  } else if (program.less) {
+    style = 'less'
+    name = getName(name,style)    
+    log(name, componentStatus, style)
+  } else {
+    name = name || process
+      .argv[2]
+      .split(',')
+    log(name, componentStatus, style)
+  }
+  console.log(name, style, usePureComponent)
+  createReactComponent(name, style, usePureComponent)
+
+  console.log('done!')
+})()
